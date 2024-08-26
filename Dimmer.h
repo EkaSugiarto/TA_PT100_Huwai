6dff2e030012d2d207fe01eb2814e70c06aa8ecf
EkaSugiarto <ekasugiarto78@gmail.com>
2024-08-27 00:30:55 +0700

    Update again hehe

--- Dimmer.h
+++ Dimmer.h
@@ -7,11 +7,12 @@ const int fanPin = 32;     // pin for fan
 
 unsigned long previousMillisssss = 0;  // will store last time the heater/cooler was updated
 
-const long heaterInterval = 100;   // Heater on duration (milliseconds)
-const long standbyInterval = 1000; // Standby duration (milliseconds)
-const long coolerInterval = 2000;  // Cooler on duration (milliseconds)
+const long heaterInterval = 50;    // Heater on duration (milliseconds)
+const long standbyInterval = 1000;  // Standby duration (milliseconds)
+const long coolerInterval = 10000;   // Cooler on duration (milliseconds)
 
-const int tempSetPoint = 50;       // Desired temperature
+const int tempSetPoint = 50;  // Desired temperature
+const int hysteresis = 2;     // Temperature buffer to avoid oscillation
 
 enum State {
   HEATING,
@@ -25,7 +26,7 @@ void heater() {
   digitalWrite(heaterPin, HIGH);
   digitalWrite(coolerPin, LOW);
   digitalWrite(fanPin, LOW);
-  Serial.println("Heater nyala, cooler mati");
+  Serial.println("Heater on, cooler off");
   Serial.println(String(Suhu1) + "  " + String(Suhu2) + "\n\n");
 }
 
@@ -33,7 +34,7 @@ void cooler() {
   digitalWrite(heaterPin, LOW);
   digitalWrite(coolerPin, HIGH);
   digitalWrite(fanPin, HIGH);
-  Serial.println("Heater mati, cooler nyala");
+  Serial.println("Heater off, cooler on");
   Serial.println(String(Suhu1) + "  " + String(Suhu2) + "\n\n");
 }
 
@@ -41,7 +42,7 @@ void standby() {
   digitalWrite(heaterPin, LOW);
   digitalWrite(coolerPin, LOW);
   digitalWrite(fanPin, LOW);
-  Serial.println("Standby ... Heater dan cooler mati");
+  Serial.println("Standby ... Heater and cooler off");
   Serial.println(String(Suhu1) + "  " + String(Suhu2) + "\n\n");
 }
 
@@ -80,60 +81,14 @@ void DimmerL() {
     default:
       if (currentMillis - previousMillisssss >= standbyInterval) {
         previousMillisssss = currentMillis;
-        if (currentTemperature < tempSetPoint) {
+        if (currentTemperature < (tempSetPoint - hysteresis)) {
           currentState = HEATING;
           heater();  // Turn on heater for 100 ms
-        } else if (currentTemperature > tempSetPoint) {
+        } else if (currentTemperature > (tempSetPoint + hysteresis)) {
           currentState = COOLING;
           cooler();  // Turn on cooler for 2000 ms
         }
       }
       break;
   }
-
-
-  // unsigned long currentMillis = millis();
-
-  // float currentTemperature = Suhu1;
-
-  // switch (currentState) {
-  //   case HEATING:
-  //     if (currentMillis - previousMillisssss >= interval) {
-  //       previousMillisssss = currentMillis;
-  //       standby();
-  //       if (currentTemperature < tempSetPoint) {
-  //         currentState = HEATING;
-  //         heater();
-  //       } else {
-  //         currentState = COOLING;
-  //       }
-  //     }
-  //     break;
-
-  //   case COOLING:
-  //     if (currentMillis - previousMillisssss >= interval) {
-  //       previousMillisssss = currentMillis;
-  //       standby();
-  //       if (currentTemperature > tempSetPoint) {
-  //         currentState = COOLING;
-  //         cooler();
-  //       } else {
-  //         currentState = HEATING;
-  //       }
-  //     }
-  //     break;
-
-  //   case STANDBY:
-  //   default:
-  //     if (currentTemperature < tempSetPoint) {
-  //       currentState = HEATING;
-  //       previousMillisssss = currentMillis;
-  //       heater();
-  //     } else if (currentTemperature > tempSetPoint) {
-  //       currentState = COOLING;
-  //       previousMillisssss = currentMillis;
-  //       cooler();
-  //     }
-  //     break;
-  // }
 }
